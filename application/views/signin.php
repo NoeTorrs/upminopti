f085ecba4c99cc5b47de56f26ec9469682c76560
Ntorres <noetrrs29@gmail.com>
2021-07-11 21:27:01 +0800

    config fix2 - noe

--- application/views/signin.php
+++ application/views/signin.php
@@ -10,6 +10,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>AMDABIDSS HEALTH Login</title>
+    <meta name="description" content="AMDABiDSS-HEALTH">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="apple-touch-icon" href="apple-icon.png">
